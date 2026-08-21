a1b530863553619d2c8625d79897dc87f3f52623
Lirake257 <152092685+Lirake257@users.noreply.github.com>
2026-08-21 18:36:16 +0300

    Update help_func.h

--- help_func.h
+++ help_func.h
@@ -21,8 +21,6 @@ void generate_e_arr_save(size_t n, std::string filename, size_t m);
 
 std::vector<long long> test10_res(const int* arr, const int &arrlen, void (*sort)(int*, size_t, long long&, long long&, long long&));
 
-std::vector<long long> test10merge_res(const int* arr, const int &arrlen, void (*sort)(int*, size_t, long long&, long long&, long long&, bool, size_t));
-
 bool sort_check(const int* arr, const int& arrlen);
 
 void res_print(int n, std::vector<std::vector<std::vector<long long>>> arr, std::string filename, bool first_call);
@@ -30,29 +28,3 @@ void res_print(int n, std::vector<std::vector<std::vector<long long>>> arr, std:
 void res100000_print(int n, std::vector<std::vector<std::vector<long long>>> arr, std::string filename);
 
 #endif
-
-
-
-
-/*
-создание массива последовательности Ли
-vector<int> generate_lee_sequence(int n) {
-    vector<int> sequence;
-    const double alpha = 2.243609061420001;
-    
-    if (n <= 0) return sequence;
-    
-    // Начинаем с 1
-    int current = 1;
-    sequence.push_back(current);
-    
-    // Генерируем последовательность пока не превысим n
-    while (current <= n) {
-        current = static_cast<int>(floor(alpha * current));
-        if (current <= n) {
-            sequence.push_back(current);
-        }
-    }
-    
-    return sequence;
-}*/
